c193fd8eefd4ea0a4ff4af1a6c807a6de1ac9189
michael <mgardner1011@gmail.com>
2022-10-25 18:12:15 -0500

    Add logic to save high score and enhance gameplay

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -11,16 +11,22 @@ class Scoreboard(Turtle):
         self.ht()
         self.goto(x=0, y=280)
         self.score = 0
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.print_score()
 
     def print_score(self):
-        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f'Score: {self.score} High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
 
     def update_score(self):
-        self.clear()
         self.score += 1
         self.print_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(str(self.high_score))
+        self.score = 0
+        self.print_score()
