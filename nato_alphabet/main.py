129e3dad88ad631821a94577625a8674ca717b1f
michael <mgardner1011@gmail.com>
2022-11-01 18:24:33 -0500

    Add Error Handling

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -7,12 +7,17 @@ is_program_on = True
 
 def make_nato():
     word = input('Enter a word: ')
-    nato_word = [nato_dict[letter.upper()] for letter in word]
-    print(nato_word)
+    try:
+        nato_word = [nato_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print('Input must contain only letters')
+        make_nato()
+    else:
+        print(nato_word)
 
 
 while is_program_on:
     make_nato()
     another_word = input('Do you have another word? (y/n): ')
-    if another_word == 'n':
+    if another_word.lower() == 'n':
         is_program_on = False
