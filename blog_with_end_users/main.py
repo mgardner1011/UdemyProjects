3effcae7254a18eb548fd6bbead033a2383ab1ed
michael <mgardner1011@gmail.com>
2022-12-18 18:02:17 -0600

    Update database to PostgreSQL

--- blog_with_end_users/main.py
+++ blog_with_end_users/main.py
@@ -12,7 +12,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -26,7 +26,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -65,7 +65,8 @@ class Comment(db.Model):
     comment_author = relationship('User', back_populates='comments')
     parent_post = relationship('BlogPost', back_populates='comments')
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
